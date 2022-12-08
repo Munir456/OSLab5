19e3966a1da46c23f4b2e23848203db3340182da
Munir Adeyola <munir.adeyola@bison.howard.edu>
2022-12-08 22:00:59 +0000

    Finished Lab 5

---
+++ ParallelMergeSort.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+#define SIZE 100
+
+int array[SIZE];
+
+void fillArrayWithRandomNumbers(int arr[SIZE]) {
+    for(int i = 0; i<SIZE; i++) array[i] = rand()%100;
+}
+
+void printArray(int arr[SIZE]) {
+    for(int i = 0; i<SIZE; i++) printf("%5d", arr[i]);
+    printf("\n");
+}
+
+/*typedef struct StartEndIndexes {
+    int start;
+    int end;
+} StartEndIndexes;
+*/
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 =  r - m;
+
+    int L[n1], R[n2];
+
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1+ j];
+
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2)
+    {
+        if (L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+// Runs mergesort on the array segment described in the
+// argument. Spawns two threads to mergesort each half
+// of the array segment, and then merges the results.
+
+//do it recursively
+void* mergeSort(int arr[], int l, int r) {
+    if (l < r)
+    {
+        int m = l+(r-l)/2;
+
+        mergeSort(arr, l, m);
+        mergeSort(arr, m+1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+void *mergeThreads(void *arg)
+{
+    int *arr = (int *)arg;
+    mergeSort(arr, 0, SIZE-1);
+}
+
+
+int main() {
+    int i;
+    pthread_t tid;
+    srand(time(NULL));
+    
+    // 1. Fill array with random numbers.
+    fillArrayWithRandomNumbers(array);
+    
+    // 2. Print the array.
+    printf("Unsorted array: ");
+    printArray(array);
+    
+    pthread_create(&tid, NULL, mergeThreads, array);
+    pthread_join(tid, NULL); 
+    
+    // 4. Wait for mergesort to finish.
+    
+    // 5. Print the sorted array.
+    printf("Sorted array:   ");
+    printArray(array);
+}
